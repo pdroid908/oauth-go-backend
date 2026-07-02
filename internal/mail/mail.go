2b9f92565ec1a61b2e37a1ef84b1b42844ce8295
pdroid908 <p1998nr@gmail.com>
2026-07-02 07:39:50 +0700

    up

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -22,7 +22,7 @@ func getResendClient() *resend.Client {
 func SendVerificationEmail(email string, token string) error {
 	client := getResendClient()
 
-	link := fmt.Sprintf("https://tujuan-baru-kamu.com/verify-email?token=%s", token)
+	link := fmt.Sprintf("https://oauth-go-backend.vercel.app/verify-email?token=%s", token)
 	body := fmt.Sprintf(`<h2>Verify Email</h2><p>Klik tombol berikut:</p><a href="%s">Verify Email</a>`, link)
 
 	params := &resend.SendEmailRequest{
@@ -39,7 +39,7 @@ func SendVerificationEmail(email string, token string) error {
 func SendResetPasswordEmail(email string, token string) error {
 	client := getResendClient()
 
-	link := fmt.Sprintf("https://tujuan-baru-kamu.com/reset-password?token=%s", token)
+	link := fmt.Sprintf("https://oauth-go-backend.vercel.app/reset-password?token=%s", token)
 	body := fmt.Sprintf(`<h2>Reset Password</h2><p>Klik tautan berikut:</p><a href="%s">Reset Password</a>`, link)
 
 	params := &resend.SendEmailRequest{
